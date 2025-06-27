761332cb633637c17947334817095b8061cdfd0b
Kalaiselvan45 <kalaiselvan@aqfer.com>
2025-06-27 16:30:07 +0530

    testing

--- aws_ci_cd/versioning-check/build.sh
+++ aws_ci_cd/versioning-check/build.sh
@@ -6,7 +6,6 @@ mkdir -p "$HOME"
 
 # # Load work area and init tools
 eval "$(ab init --mkdirs build)"
-ab go-cache get
 
 # Get semver version (e.g. v1.2.3)
 export IMAGE_TAG=$(ab semver get | tail -n1)
