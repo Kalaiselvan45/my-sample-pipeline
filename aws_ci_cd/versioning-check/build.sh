a2b0e8449b0bceecc49cc3e240e7c9b39c77822d
Kalaiselvan45 <kalaiselvan@aqfer.com>
2025-06-27 15:44:56 +0530

    testing

--- aws_ci_cd/versioning-check/build.sh
+++ aws_ci_cd/versioning-check/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-set -euo pipefail
+set -ex
 
-export HOME=/tmp/home
-mkdir -p "$HOME"
+# export HOME=/tmp/home
+# mkdir -p "$HOME"
 
-# Load work area and init tools
-eval "$(ab init --mkdirs build)"
-ab go-cache get
+# # Load work area and init tools
+# eval "$(ab init --mkdirs build)"
+# ab go-cache get
 
 # Get semver version (e.g. v1.2.3)
-export IMAGE_TAG=$(ab semver get | tail -n1)
-echo "Using image tag: $IMAGE_TAG"
+# export IMAGE_TAG=$(ab semver get | tail -n1)
+# echo "Using image tag: $IMAGE_TAG"
 
 # Build Go binary
-echo "Building Go binary..."
-go build -o myself
+# echo "Building Go binary..."
+
 
 # Docker login
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$ECR_REPO"
@@ -25,4 +25,4 @@ docker push "$ECR_REPO:$IMAGE_TAG"
 docker push "$ECR_REPO:latest"
 
 # Put semver (confirm version used)
-ab semver put
+# ab semver put
