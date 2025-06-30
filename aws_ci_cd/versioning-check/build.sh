9dad812979b9754e53b6c0e64a1be82e795697c3
Kalaiselvan45 <kalaiselvan@aqfer.com>
2025-06-30 13:26:01 +0530

    testing

--- aws_ci_cd/versioning-check/build.sh
+++ aws_ci_cd/versioning-check/build.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
-set -ex
+set -ex  # Exit on any error
 
+PS4='+ $(date "+%Y/%m/%d %H:%M:%S")  '
+
+# /tmp is used as only this is writable in Lambda
+mkdir -p /tmp/home
+chmod 700 /tmp/home
 export HOME=/tmp/home
-mkdir -p "$HOME"
+mkdir -p $HOME/downloads $HOME/build/bin $HOME/.ssh $HOME/gopath
+export PATH="$HOME/build/bin:$HOME/build/go/bin:$PATH"
 
-# # Load work area and init tools
+export GOPATH=$HOME/gopath
+# Install Go
+curl -fsSL "https://dl.google.com/go/go${GO_VERSION}.linux-${ARCH}.tar.gz" | tar -C $HOME/build -xz
+go version
+
+echo "Setting up Git SSH configuration..."
+export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa -o UserKnownHostsFile=$HOME/.ssh/known_hosts"
+
+cat > $HOME/.ssh/known_hosts <<EOF
+$CI_GITHUB_SSH_RSA
+EOF
+
+cat > $HOME/.ssh/id_rsa <<GIT
+$CI_GITHUB_SSH_PRIVATE_KEY
+GIT
+
+chmod 0700 $HOME/.ssh
+chmod 0600 $HOME/.ssh/known_hosts ~/.ssh/id_rsa
+git config --global url."git@github.com:".insteadof "https://github.com/"
+echo "Git setup completed."
+
+(
+  cd "$CODEBUILD_SRC_DIR/spark/aws-ci-cd/cost-estimator"
+  go build -o "$HOME/build/bin/cost-estimator" .
+)
+
+# Install uber-ab
+(
+  cd "$CODEBUILD_SRC_DIR/spark/aws-ci-cd/amdp-builder/cmd/uber-ab"
+  go build -o "$HOME/build/bin/uber-ab" .
+  ln -s "$HOME/build/bin/uber-ab" "$HOME/build/bin/ab"
+)
+
 # eval "$(ab init --mkdirs build)"
 echo $CODEBUILD_RESOLVED_SOURCE_VERSION
 
