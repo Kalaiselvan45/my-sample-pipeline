8b06b461457d60ce9cdbc5bbdc943bcbdf7874b4
Kalaiselvan45 <kalaiselvan@aqfer.com>
2025-07-02 12:02:06 +0530

    testing

--- aws_ci_cd/versioning-check/build.sh
+++ aws_ci_cd/versioning-check/build.sh
@@ -30,7 +30,6 @@ aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS 
 # Build and tag Docker image
 docker build \
   -f "$CODEBUILD_SRC_DIR/aws_ci_cd/versioning-check/dockerfile" \
-  -t "$ECR_REPO:$IMAGE_TAG" \
   -t "$ECR_REPO:latest" \
   "$CODEBUILD_SRC_DIR"
 
